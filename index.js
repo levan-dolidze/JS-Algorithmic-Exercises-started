af58e011704e852ecf19d5072cfd02f86b2f287c
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2020-08-23 14:57:04 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+
+
+
+// const diagonali1=0;
+// const diagonali2=0
+// const array=[
+// [11,2,4],
+// [4,5,6],
+// [10,8,-12],
+// ];
+
+// for (let index = 0; index < array.length; index++) {
+//     for (let j= 0; j < array.length; j++) {
+        
+        
+    
+// diagonali1.push(array[0][0])
+// diagonali1.push(array[1][1])
+// diagonali1.push(array[2][2])
+// diagonali2.push(array[0][2])
+// diagonali2.push(array[1][1])
+// diagonali2.push(array[2][0])
+// }
+// }
+// const sxvaoba=diagonali1-diagonali2;
+// console.log(sxvaoba);
+
+
+
+
+// const array = [[11,2,4],
+// [4,5,6],
+// [10,8,-12]
+// ];
+
+// function task(arr) {
+//     let sum = 0;
+//     for (var i = 0; i <= arr.length - 1; i++) {
+//         sum += arr[i][i] - arr[i].reverse()[i];
+//     }
+//     return Math.abs(sum);
+// }
+// console.log(task(array));
+
+
+
+// const array=[1,1,0,-1,3,6];
+
+// const mteli=0;
+// const nuli=0;
+// const minusi=0;
+
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index]>0){
+//         mteli+=array[index];
+//     }
+//     else if(array[index]<0){
+//         minusi+=array[index];
+//     }
+//     else{
+//         nuli+=array[index];
+//     }
+ 
+// }
+
+// const shedegi=mteli/array.length;
+// const shedegi2=nuli/array.length;
+// const shedegi3=minusi/array.length;
+
+// console.log(shedegi,shedegi2,shedegi2);
+
+
+
+// const array1=[35,58,75,77,33,38];
+
+// const array2=[35,60,75,80,33,40];
+
+// for (let index = 0; index < array1.length; index++) {
+ 
+//     if(array1[0]<= 40 ) {
+//         console.log('yop')
+//     }
+
+//     if(array1[1]>=58&&array1[1]<=60){
+//         console.log(array2[1])
+//     }
+//     if(array1[2]==75){
+//         console.log(array2[2])
+//     }
+//     if(array1[3]>=77&&array1[3]<=80){
+//         console.log(array2[3])
+//     }
+   
+
+
+// }
+const key=3;
+
+const array1=[35,58,75,77,33,38];
+
+const array2=[35,60,75,80,33,40];
+
+const ss=array2[0]-array1[0]
+console.log(ss)
+
+// for (let i= 0; i< array1.length; i++) {
+   
+    
+// }
+
+// for (let j = 0; j < array2.length; j++) {
+    
+
+// }
+// const sxvaoba=[]
+// sxvaoba= (array1[i]-array2[j])
+//     console.log(sxvaoba)
